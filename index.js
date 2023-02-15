e79874e43481b2d9ad564c1bcef32fdf9d42b1ed
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-02-15 15:06:36 +0100

    API data loading

--- index.js
+++ index.js
@@ -7,8 +7,36 @@ app.set('views', './views')
 
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-    res.render('index')
+const url = "https://whois.fdnd.nl/api/v1/"
+const repos = "https://api.github.com/users/"
+
+async function dataFetch(url) {
+    const data = await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+
+    return data
+}
+
+app.get('/', async (req, res) => {
+
+    const addonUrl = url + "members?first=100"
+    const data = await dataFetch(addonUrl)
+    // const repo = await dataFetch(repos)
+
+    data.members.forEach(member => {
+        console.log(member.gitHubHandle)
+    });
+    
+    res.render('index', { data })
+})
+
+app.get('/squad-a', async (req, res) => {
+
+    const addonUrl = url + "squads/squad-a-2022/"
+    const data = await dataFetch(addonUrl)
+
+    res.render('index', { data })
 })
 
 app.set('port', process.env.PORT || 8000)
