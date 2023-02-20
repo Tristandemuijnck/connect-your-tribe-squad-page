4aa4dd5c3bfe25931fa7705118c5624237aeeb4f
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-02-20 01:14:36 +0100

    Added members page + filtering

--- index.js
+++ index.js
@@ -11,19 +11,25 @@ app.set('views', './views')
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get('/', async (req, res) => {
 
     const addonUrl = urlMember + "members?first=100"
     const data = await dataFetch(addonUrl)
-    
+
     data.members.forEach(member => {
         // Random number generator
         member.publicRepos = Math.floor(Math.random() * 20)
     });
 
-    res.render('index', { data })
+    console.log(data)
+
+    res.render('index', {
+        data
+    })
 })
 
 app.get('/FDND', async (req, res) => {
@@ -40,7 +46,7 @@ app.get('/FDND', async (req, res) => {
             sortingFilter = "name"
             direction = "ASC"
             break;
-        
+
         case "name-ZA":
             sortingFilter = "name"
             direction = "DESC"
@@ -52,7 +58,7 @@ app.get('/FDND', async (req, res) => {
         case "student":
             roleFilter = "student"
             break;
-        
+
         case "lecturer":
             roleFilter = "lecturer"
             break;
@@ -61,19 +67,6 @@ app.get('/FDND', async (req, res) => {
             roleFilter = "co_teacher"
             break;
     }
-
-    // Construct correct API url based on chosen filters
-
-    // Check if role or cohort filters are being used
-    // if (roleFilter == undefined && cohortFilter == undefined) {
-    //     filterUrl = urlSquad + slug  + '?orderBy=' + sortingFilter + '&direction=' + direction
-    // } else if(roleFilter !== undefined && cohortFilter == undefined){
-    //     filterUrl = urlMember + "members?first=100" + '&role=' + roleFilter + '&orderBy=' + sortingFilter + '&direction=' + direction
-    // } else if(roleFilter == undefined && cohortFilter !== undefined){
-    //     filterUrl = urlMember + "members?first=100" + '&cohort=' + cohortFilter + '&orderBy=' + sortingFilter + '&direction=' + direction
-    // } else if(roleFilter !== undefined && cohortFilter !== undefined){
-    //     filterUrl = urlMember + "members?first=100" + '&role=' + roleFilter + '&cohort=' + cohortFilter + '&orderBy=' + sortingFilter + '&direction=' + direction
-    // }
 
     let squadUrl = urlSquad + slug
 
@@ -85,7 +78,50 @@ app.get('/FDND', async (req, res) => {
         member.publicRepos = Math.floor(Math.random() * 20)
     });
 
-    res.render('squads', data);
+    res.render('squads', data)
+})
+
+// Test using filter() method
+app.get('/members', async (req, res) => {
+
+    // Base url for API call
+    const testUrl = "https://whois.fdnd.nl/api/v1/members?first=100"
+
+    // API call for fetching data
+    const data = await dataFetch(testUrl)
+
+    // Filter data for members based on chosen filters
+    const dataDisplay = data.members.filter((eventData) => {
+
+        // console.log(req.query)
+        // console.log(eventData.role)
+
+        // If no squad filter is chosen, check if role filter is chosen. Else, check if squad filter matches with data
+        if (!req.query.squad) {
+            // If no role filter is chosen, check if cohort filter is chosen. Else, check if role filter matches with data
+            if (!req.query.role) {
+                // If no cohort filter is chosen, return all data. Else, check if cohort filter matches with data
+                if (!req.query.cohort) {
+                    return eventData;
+                }else if (eventData.squads[0].cohort.toLowerCase().includes(req.query.cohort.toLowerCase())) { 
+                    return eventData;
+                }
+            } else if (eventData.role.includes(req.query.role.toLowerCase())) {
+                return eventData;
+            }
+        } else if (eventData.squads[0].slug.toLowerCase().includes(req.query.squad.toLowerCase())) {
+            return eventData;
+        }
+    })
+
+    dataDisplay.forEach(member => {
+        // Random number generator
+        member.publicRepos = Math.floor(Math.random() * 20)
+    });
+
+    res.render('members', {
+        dataDisplay
+    })
 })
 
 app.set('port', process.env.PORT || 8000)
@@ -96,8 +132,8 @@ app.listen(app.get('port'), function () {
 
 async function dataFetch(url) {
     const data = await fetch(url)
-    .then((response) => response.json())
-    .catch((error) => error)
+        .then((response) => response.json())
+        .catch((error) => error)
 
     return data
 }
